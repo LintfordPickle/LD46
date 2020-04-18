a41edf8a7662a76ef800e9bace2f102cbbf86206
John Hampson <jonnohampson@gmail.com>
2020-04-18 05:00:45 +0200

    Added basic build file (ant) and app settings

--- src/net/lintford/ld46/BaseGame.java
+++ src/net/lintford/ld46/BaseGame.java
@@ -6,11 +6,28 @@ import net.lintford.library.core.LintfordCore;
 public class BaseGame extends LintfordCore {
 
 	// ---------------------------------------------
+	// Constants
+	// ---------------------------------------------
+
+	public static final String APPLICATION_NAME = "Ludum Dare #46";
+	public static final String WINDOW_TITLE = "Ludum Dare #46 © John Hampson";
+
+	// ---------------------------------------------
 	// Entry Point
 	// ---------------------------------------------
 
 	public static void main(String[] pArgs) {
 		GameInfo lGameInfo = new GameInfo() {
+			@Override
+			public String applicationName() {
+				return APPLICATION_NAME;
+			}
+
+			@Override
+			public String windowTitle() {
+				return WINDOW_TITLE;
+			}
+
 		};
 
 		final var lBaseGame = new BaseGame(lGameInfo, pArgs);
